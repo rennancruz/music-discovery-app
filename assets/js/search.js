b624cb08eb373d497dd8678100287f07ac9f7224
Rennan Cruz <ren1908@icloud.com>
2024-09-19 19:31:42 -0700

    Adding search feature

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,13 +2,11 @@ async function searchMusic() {
   const searchInput = document.getElementById("search-input").value;
   const resultsContainer = document.getElementById("results");
   resultsContainer.innerHTML = "";
-
   if (searchInput.trim() === "") {
     resultsContainer.innerHTML =
       "<p class=''>Please enter a song or artist's name.</p>";
     return;
   }
-
   const itunesResponse = await fetch(
     `https://itunes.apple.com/search?term=${encodeURIComponent(
       searchInput
@@ -16,19 +14,16 @@ async function searchMusic() {
   );
   const itunesData = await itunesResponse.json();
   console.log(itunesData);
-
   if (itunesData.results.length === 0) {
     resultsContainer.innerHTML = '<p class="">No Results Found.</p>';
   } else {
     // Remove duplicates by using a Map to filter unique track and artist combinations
     const uniqueResults = new Map();
-
     itunesData.results.forEach((result) => {
-      const uniqueKey = `${result.trackName}-${result.artistName}`.toLowerCase();
-
+      const uniqueKey =
+        `${result.trackName}-${result.artistName}-${result.collectionName}`.toLowerCase();
       if (!uniqueResults.has(uniqueKey)) {
         uniqueResults.set(uniqueKey, result);
-
         const songElement = document.createElement("div");
         songElement.classList.add(
           "p-4",
@@ -39,16 +34,17 @@ async function searchMusic() {
           "items-center",
           "mb-2"
         );
-
         songElement.innerHTML = `
-          <div class="text-left"> 
-              <p class="text-gray-800">${result.trackName}</p> 
-              <p class="text-sm text-gray-800">${result.artistName}</p> 
+          <div class="text-left">
+              <p class="text-lg text-gray-800">${result.trackName}</p>
+              <p class="text-md text-gray-800">${result.artistName}</p>
+              </br>
+              <p class="text-sm text-gray-800">${result.collectionName}</p>
           </div>
           <button id="addPlaylist" class="bg-blue-500 rounded px-3 py-1 flex text-sm text-white hover:bg-blue-600" style="flex-shrink:0;width:100px;" onclick='addToPlaylist(${JSON.stringify(
             result
-          )})'> 
-              <i class="fas fa-plus text-gray-900 mt-3.5"></i> Add To Playlist 
+          )})'>
+              <i class="fas fa-plus text-gray-900 mt-3.5"></i> Add To Playlist
           </button>
         `;
         resultsContainer.appendChild(songElement);
@@ -56,35 +52,32 @@ async function searchMusic() {
     });
   }
 }
-
 function addToPlaylist(song) {
   const playlist = JSON.parse(localStorage.getItem("playlist")) || [];
-
   // Check if the song already exists in the playlist
   const songExists = playlist.some(
-    (track) => track.trackName === song.trackName && track.artistName === song.artistName
+    (track) =>
+      track.trackName === song.trackName && track.artistName === song.artistName
   );
-
   if (!songExists) {
     playlist.push({
       trackName: song.trackName,
       artistName: song.artistName,
+      collectionName: song.collectionName,
     });
     localStorage.setItem("playlist", JSON.stringify(playlist));
-
     // Create the modal container
     const modalContainer = document.createElement("div");
     modalContainer.id = "modal"; // Set an ID for easy removal
     modalContainer.className =
       "fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"; // Add styles for modal backdrop
-
     // Create the modal content
     modalContainer.innerHTML = `
       <div class="bg-white p-4 rounded shadow-lg">
         <!-- Modal Header -->
         <div class="flex justify-between items-center">
           <h2 class="text-lg font-semibold">Song Added To Playlist!</h2>
-          <button class="text-gray-500 hover:text-gray-800" onclick="closeModal()">✖</button>
+          <button class="text-gray-500 hover:text-gray-800" onclick="closeModal()">:heavy_multiplication_x:</button>
         </div>
         <!-- Modal Content -->
         <p>${song.trackName} was added to your playlist!</p>
@@ -94,14 +87,14 @@ function addToPlaylist(song) {
         </div>
       </div>
     `;
-
     // Append the modal to the body
     document.body.appendChild(modalContainer);
   } else {
-    alert(`${song.trackName} by ${song.artistName} is already in your playlist.`);
+    alert(
+      `${song.trackName} by ${song.artistName} is already in your playlist.`
+    );
   }
 }
-
 // Function to close the modal
 function closeModal() {
   const modal = document.getElementById("modal");
